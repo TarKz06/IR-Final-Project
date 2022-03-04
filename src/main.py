0df753f9678993ad8dec455adadc32c7332ca6e1
Prin Ritti-traiphop <prinr2481@gmail.com>
2022-03-04 12:10:12 +0700

    Search tf-idf of Ingredient

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import string
-
+import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def get_and_clean_data():
@@ -34,3 +36,30 @@ def get_and_clean_data():
     gen_clean_csv = {"Number": num,"Title": cleaned_title ,"Instructions":cleaned_instructions,"Image_Name": image_name,"Ingredients": cleaned_ingredients}
     df = pd.DataFrame(data=gen_clean_csv)
     df.to_csv("src/resources/new Food Ingredients and Recipe.csv", encoding="utf8", index=False)
+
+# Read csv file
+cleanData = pd.read_csv('src/resources/new Food Ingredients and Recipe.csv')
+
+#Search TF-IDF
+def tf_idfByIng(Input):
+    vectorizer = TfidfVectorizer()
+    data_new = pd.DataFrame(cleanData, columns=['Title','Ingredients','Instructions','Image_Name'])
+    findTarg = vectorizer.fit_transform(cleanData['Ingredients'].apply(lambda x: np.str_(x)))
+    query_vec = vectorizer.transform([Input])
+    results = cosine_similarity(findTarg, query_vec).reshape((-1,))
+    count = 0
+    dataTfidf = []
+    for i in results.argsort()[:][::-1]:
+        if (results[i]>0.1):
+            count += 1
+            dataTfidf.append({
+                "Number": count,
+                "Title": data_new.iloc[i, 0],
+                "Ingredients": data_new.iloc[i, 1],
+                "Instructions": data_new.iloc[i, 2],
+                "Image_Name": data_new.iloc[i, 3],
+                "Score": results[i]
+            })
+
+    return dataTfidf
+
