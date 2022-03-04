bbd07b319a2cc6bccba0673329536e096d2bf53f
TarKz06 <khematat_s@elearning.cmu.ac.th>
2022-03-04 11:53:51 +0700

    tf-idf search by title and show score

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import string
 
+import numpy as np
 import pandas as pd
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def get_and_clean_data():
 
@@ -39,3 +41,26 @@ def get_and_clean_data():
         print('New csv : new Food Ingredients and Recipe.csv')
     except:
         print("Error")
+
+def tfidf_scoring_by_title(query):
+
+    csv_file = "src/resources/new Food Ingredients and Recipe.csv"
+    data = pd.read_csv(csv_file)
+    all_data = pd.DataFrame(data, columns=['Title', 'Image_Name', 'Ingredients'])
+    tfidf_data_ranking = []
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(all_data['Title'].apply(lambda x: np.str_(x)))
+    query_vectorizer = vectorizer.transform([query])
+    results = cosine_similarity(X, query_vectorizer).reshape((-1,))
+    rank = 0
+    for i in results.argsort()[-10:][::-1]:
+        rank = rank + 1
+        tfidf_data_ranking.append({
+            "Rank": rank,
+            "Title": all_data.iloc[i, 0],
+            "Image": all_data.iloc[i, 1],
+            "Ingredient": all_data.iloc[i, 2],
+            "Score": results[i]
+            }
+        )
+    return tfidf_data_ranking
