a455c873e296367e351f7d27202501150d0ee7d6
TarKz06 <khematat_s@elearning.cmu.ac.th>
2022-03-06 20:19:18 +0700

    Move backend in project folder and config

---
+++ src/project/backend.py
@@ -0,0 +1,47 @@
+import string
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def get_and_clean_data():
+
+    data = pd.read_csv('src/resources/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
+
+    # Number id
+    num = data['Number']
+
+    # Title
+    title = data['Title'].astype(str)
+    cleaned_title = title.apply(lambda s: s.translate(str.maketrans ('','', '([$\'_&+,:;=?@\[\]#|<>.^*()%\\!"-])' +U'\xa8')))
+    cleaned_title = cleaned_title.apply(lambda s: s.lower())
+    cleaned_title = cleaned_title.apply(lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    # Instructions
+    instructions = data['Instructions'].astype(str)
+    cleaned_instructions = instructions.apply(lambda s: s.translate(str.maketrans ('','', '([$\'_&+,:;=?@\[\]#|<>.^*()%\\!"-])' +U'\xa8')))
+    cleaned_instructions = cleaned_instructions.apply(lambda s: s.lower())
+    cleaned_instructions = cleaned_instructions.apply(lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    # Image_name
+    image_name = data['Image_Name'].astype(str)
+
+    # Ingredients
+    ingredients = data['Cleaned_Ingredients'].astype(str)
+    cleaned_ingredients = ingredients.apply(lambda s: s.translate(str.maketrans ('','', '([$\'_&+,:;=?@\[\]#|<>.^*()%\\!"-])' +U'\xa8')))
+    cleaned_ingredients = cleaned_ingredients.apply(lambda s: s.lower())
+    cleaned_ingredients = cleaned_ingredients.apply(lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    gen_clean_csv = {"Number": num,"Title": cleaned_title ,"Instructions":cleaned_instructions,"Image_Name": image_name,"Ingredients": cleaned_ingredients}
+    df = pd.DataFrame(data=gen_clean_csv)
+
+    df.to_csv("src/resources/new Food Ingredients and Recipe.csv", encoding="utf8", index=False)
+
+    try:
+        df.to_csv("src/resources/new Food Ingredients and Recipe.csv", encoding="utf8", index=False)
+        print('New csv : new Food Ingredients and Recipe.csv')
+    except:
+        print("Error")
+
+
--- src/project/main.py
+++ src/project/main.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, render_template 
 from flask_login import login_required, current_user
 
+from .backend import get_and_clean_data, exampleoutput
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/test')
+def test():
+    return render_template('test.html',)
 
 @main.route('/profile')
 @login_required
